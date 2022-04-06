22050b222f49168fe1c0a3689ce254dc49cf1bf2
tagucha <tagucha.dev@gmail.com>
2022-04-06 22:40:15 +0900

    Revert "First commit"
    This reverts commit 2c9b260307e81fa80b6220f2564aef06a9786a6f.

--- src/main/java/net/tagucha/jrpg/exception/GameAreaInputException.java
+++
@@ -1,9 +0,0 @@
-package net.tagucha.jrpg.exception;
-
-import net.tagucha.jrpg.JinroGame;
-
-public class GameAreaInputException extends GameException{
-    public GameAreaInputException(JinroGame game, String message) {
-        super(game,message);
-    }
-}
--- src/main/java/net/tagucha/jrpg/exception/PermissionException.java
+++
@@ -1,9 +0,0 @@
-package net.tagucha.jrpg.exception;
-
-import org.bukkit.ChatColor;
-
-public class PermissionException extends SimpleMessageException {
-    public PermissionException() {
-        super(ChatColor.RED,ChatColor.RED,"権限がありません。");
-    }
-}
